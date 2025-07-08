3d09589ab20274dce93107ed3d93a2f89f75ecf0
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-07-09 00:05:45 +0700

    Add Dashboard controller & index view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,5 @@ Route::get('/', function () {
 
 Route::get('/google/auth', [AuthController::class, 'googleAuth'])->name('login');
 Route::get('/google/callback', [AuthController::class, 'googleCallback']);
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
