0d63b37cc63d17526dc12a4649e1a14b4c72984f
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-07-19 15:58:58 +0700

    Handling failed response in google service

--- app/Services/Google/GoogleService.php
+++ app/Services/Google/GoogleService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Google;
 
+use App\Exceptions\GoogleException;
 use Illuminate\Support\Facades\Http;
 
 class GoogleService
@@ -25,32 +26,39 @@ class GoogleService
 
     public function getToken(string $code)
     {
-        $response = Http::asForm()->throw()->post(self::GOOGLE_API_TOKEN_URL, [
+        $response = Http::asForm()->post(self::GOOGLE_API_TOKEN_URL, [
             'client_id' => env('GOOGLE_CLIENT_ID'),
             'client_secret' => env('GOOGLE_CLIENT_SECRET'),
             'code' => $code,
             'grant_type' => 'authorization_code',
             'redirect_uri' => env('GOOGLE_REDIRECT_URL')
         ]);
+        if ($response->failed()) {
+            throw new GoogleException($response);
+        }
         return $response->collect();
     }
 
     public function refreshToken(string $token)
     {
-        $response = Http::asForm()->throw()->post(self::GOOGLE_API_TOKEN_URL, [
+        $response = Http::asForm()->post(self::GOOGLE_API_TOKEN_URL, [
             'client_id' => env('GOOGLE_CLIENT_ID'),
             'client_secret' => env('GOOGLE_CLIENT_SECRET'),
             'grant_type' => 'refresh_token',
             'refresh_token' => $token
         ]);
-        $response->throw();
+        if ($response->failed()) {
+            throw new GoogleException($response);
+        }
         return $response->collect();
     }
 
     public function getAccountInfo(string $accessToken)
     {
-        $response = Http::withToken($accessToken)
-            ->throw()->get(self::GOOGLE_USER_INFO_URL);
+        $response = Http::withToken($accessToken)->get(self::GOOGLE_USER_INFO_URL);
+        if ($response->failed()) {
+            throw new GoogleException($response);
+        }
         return $response->collect();
     }
 }
