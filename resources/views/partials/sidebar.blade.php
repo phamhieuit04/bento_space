6fc9cc6915c0e785d6578a141a49ee7633ddf700
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-08-05 22:52:34 +0700

    Update sidebar.blade

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,13 +1,23 @@
 <flux:sidebar sticky stashable class="w-96">
     <flux:navlist class="w-full gap-1">
-        <flux:navlist.item
-            icon="folder"
-            icon:variant="solid"
-            href="{{ url('/drive/dashboard') }}"
-            class="h-12!"
-        >
-            Google Drive
-        </flux:navlist.item>
+        <flux:navlist.group heading="Google Drive" expandable>
+            <flux:navlist.item
+                icon="folder"
+                icon:variant="solid"
+                class="my-2! h-12!"
+                href="{{ url('/drive/dashboard') }}"
+            >
+                Dashboard
+            </flux:navlist.item>
+            <flux:navlist.item
+                href="{{ url('/drive/trash') }}"
+                icon="trash"
+                icon:variant="solid"
+                class="my-2! h-12!"
+            >
+                Trash bin
+            </flux:navlist.item>
+        </flux:navlist.group>
         <flux:navlist.item
             icon="pencil"
             icon:variant="solid"
