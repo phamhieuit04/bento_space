b20ab99a2bd13ba17526472b225cb1664bd17d49
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-08-10 21:41:39 +0700

    Refactor file repository

--- app/Repositories/File/FileRepository.php
+++ app/Repositories/File/FileRepository.php
@@ -15,32 +15,24 @@ class FileRepository extends BaseRepository implements FileRepositoryInterface
         return File::class;
     }
 
-    public function all()
-    {
-        return $this->model->where('user_id', Auth::id())
-            ->where('trashed', TrashedStatus::NOT_TRASHED->value)
-            ->orderBy('created_at', 'desc')->get();
-    }
-
-    public function starred()
-    {
-        return $this->model->where('user_id', Auth::id())
-            ->where('starred', StarredStatus::STARRED->value)
-            ->where('trashed', TrashedStatus::NOT_TRASHED->value)
-            ->orderBy('updated_at', 'desc')->get();
-    }
-
-    public function trashed()
-    {
-        return $this->model->where('user_id', Auth::id())
-            ->where('trashed', TrashedStatus::TRASHED->value)
-            ->orderBy('updated_at', 'desc')->get();
-    }
-
     public function search(string $column, $value)
     {
         $collection = $this->model->where($column, 'like', "%$value%")
             ->where('trashed', TrashedStatus::NOT_TRASHED->value)->get();
         return $collection;
+    }
+
+    public function filter(string $filter)
+    {
+        $collection = $this->model->where('user_id', Auth::id());
+        return match ($filter) {
+            'starred' => $collection->where('starred', StarredStatus::STARRED->value)
+                ->where('trashed', TrashedStatus::NOT_TRASHED->value)
+                ->orderBy('updated_at', 'desc')->get(),
+            'trashed' => $collection->where('trashed', TrashedStatus::TRASHED->value)
+                ->orderBy('updated_at', 'desc')->get(),
+            'all' =>  $collection->where('trashed', TrashedStatus::NOT_TRASHED->value)
+                ->orderBy('created_at', 'desc')->get()
+        };
     }
 }
--- app/Repositories/File/FileRepositoryInterface.php
+++ app/Repositories/File/FileRepositoryInterface.php
@@ -4,11 +4,7 @@ namespace App\Repositories\File;
 
 interface FileRepositoryInterface
 {
-    public function all();
-
-    public function starred();
-
-    public function trashed();
-
     public function search(string $column, string $value);
+
+    public  function filter(string $filter);
 }
