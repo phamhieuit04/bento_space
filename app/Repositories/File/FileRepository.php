5cd0bceb859c514219ecb4a6d580d69b4d363826
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-08-10 23:56:12 +0700

    Update file repository

--- app/Repositories/File/FileRepository.php
+++ app/Repositories/File/FileRepository.php
@@ -22,17 +22,18 @@ class FileRepository extends BaseRepository implements FileRepositoryInterface
         return $collection;
     }
 
-    public function filter(string $filter)
+    public function filter(string $filter, array $orderBy = null)
     {
         $collection = $this->model->where('user_id', Auth::id());
-        return match ($filter) {
+        $result = match ($filter) {
             'starred' => $collection->where('starred', StarredStatus::STARRED->value)
-                ->where('trashed', TrashedStatus::NOT_TRASHED->value)
-                ->orderBy('updated_at', 'desc')->get(),
-            'trashed' => $collection->where('trashed', TrashedStatus::TRASHED->value)
-                ->orderBy('updated_at', 'desc')->get(),
-            'all' =>  $collection->where('trashed', TrashedStatus::NOT_TRASHED->value)
-                ->orderBy('created_at', 'desc')->get()
+                ->where('trashed', TrashedStatus::NOT_TRASHED->value),
+            'trashed' => $collection->where('trashed', TrashedStatus::TRASHED->value),
+            'all' => $collection->where('trashed', TrashedStatus::NOT_TRASHED->value),
         };
+        !blank($orderBy) ?
+            $result->orderBy($orderBy[0], $orderBy[1]) :
+            $result->orderBy('created_at', 'desc');
+        return $result->get();
     }
 }
