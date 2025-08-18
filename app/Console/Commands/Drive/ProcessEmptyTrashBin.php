77eb6c46faa1c3e481b1f2ed8dc97952e3ff3188
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-08-18 22:43:46 +0700

    Add process empty trash bin command

---
+++ app/Console/Commands/Drive/ProcessEmptyTrashBin.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands\Drive;
+
+use App\Enums\Drive\TrashedStatus;
+use App\Repositories\File\FileRepository;
+use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class ProcessEmptyTrashBin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'drive:process-empty-trash-bin';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Perform empty google drive trashbin after 30 days';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(FileRepository $fileRepo)
+    {
+        DB::transaction(function () use ($fileRepo) {
+            foreach ($fileRepo->all() as $item) {
+                if (
+                    $item->updated_at->addDays(30)->lessThan(now())
+                    && $item->trashed == TrashedStatus::TRASHED->value
+                ) {
+                    $fileRepo->delete($item->id);
+                    Log::info("Successfully delete file [{$item->drive_id}, {$item->name}] (trashed more than 30 days).");
+                }
+            }
+        });
+    }
+}
